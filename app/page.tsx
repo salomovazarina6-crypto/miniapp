7d4ecdd9289c947981e72074b7460141dcf908fc
salomovazarina6-crypto <salomovazarina6@gmail.com>
2026-02-16 00:53:16 +0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,3 @@
 export default function Page() {
-  return (
-    <main style={{ padding: 24, fontFamily: "Arial" }}>
-      <h1>Mega MiniApp PRO</h1>
-      <p>Root page ishlayapti ✅</p>
-    </main>
-  );
+  return <h1>HELLO MINIAPP</h1>;
 }
